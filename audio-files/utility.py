e20dfe86a98a2f516b0617de13017d5af5b23e95
Matthias Geier <Matthias.Geier@gmail.com>
2014-09-13 11:49:16 +0200

    Some changes to audio file howtos

--- audio-files/utility.py
+++ audio-files/utility.py
@@ -1,21 +1,19 @@
-"""
-Helper functions for working with audio files in NumPy.
-"""
+"""Helper functions for working with audio files in NumPy."""
 
 import numpy as np
 import contextlib
 
-def pcm2float(sig, dtype=np.float64):
-    """
-    Convert PCM signal to floating point with a range from -1 to 1.
 
-    Use dtype=np.float32 for single precision.
+def pcm2float(sig, dtype='float64'):
+    """Convert PCM signal to floating point with a range from -1 to 1.
+
+    Use dtype='float32' for single precision.
 
     Parameters
     ----------
     sig : array_like
         Input array, must have (signed) integral type.
-    dtype : data-type, optional
+    dtype : data type, optional
         Desired (floating point) data type.
 
     Returns
@@ -25,26 +23,61 @@ def pcm2float(sig, dtype=np.float64):
 
     See Also
     --------
-    dtype
+    float2pcm, dtype
 
     """
     # TODO: allow unsigned (e.g. 8-bit) data
 
-    sig = np.asarray(sig)  # make sure it's a NumPy array
-    assert sig.dtype.kind == 'i', "'sig' must be an array of signed integers!"
-    dtype = np.dtype(dtype)  # allow string input (e.g. 'f')
+    sig = np.asarray(sig)
+    if sig.dtype.kind != 'i':
+        raise TypeError("'sig' must be an array of signed integers")
+    dtype = np.dtype(dtype)
+    if dtype.kind != 'f':
+        raise TypeError("'dtype' must be floating point type")
 
     # Note that 'min' has a greater (by 1) absolute value than 'max'!
-    # Therefore, we use 'min' here to avoid clipping.
+    # Therefore, we use '-min' here to avoid clipping.
     return sig.astype(dtype) / dtype.type(-np.iinfo(sig.dtype).min)
+
+
+def float2pcm(sig, dtype='int16'):
+    """Convert floating point signal with a range from -1 to 1 to PCM.
+
+    Parameters
+    ----------
+    sig : array_like
+        Input array, must have floating point type.
+    dtype : data type, optional
+        Desired (integer) data type.
+
+    Returns
+    -------
+    ndarray
+        integer data.
+
+    See Also
+    --------
+    pcm2float, dtype
+
+    """
+    # TODO: allow unsigned (e.g. 8-bit) data
+
+    sig = np.asarray(sig)
+    if sig.dtype.kind != 'f':
+        raise TypeError("'sig' must be a float array")
+    dtype = np.dtype(dtype)
+    if dtype.kind != 'i':
+        raise TypeError("'dtype' must be signed integer type")
+
+    return (sig * np.iinfo(dtype).max).astype(dtype)
 
 
 @contextlib.contextmanager
 def printoptions(*args, **kwargs):
-    """
-    Context manager for temporarily setting NumPy print options.
+    """Context manager for temporarily setting NumPy print options.
 
     See http://stackoverflow.com/a/2891805/500098
+
     """
     original = np.get_printoptions()
     try:
@@ -52,4 +85,3 @@ def printoptions(*args, **kwargs):
         yield
     finally:
         np.set_printoptions(**original)
-
